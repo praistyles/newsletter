ecd8c70eddfe6707b45e75fa59428280e930d038
Praise Magoche <praisemagoche@gmail.com>
2025-08-05 12:26:27 +0200

    Add files via upload

---
+++ newsletter.js
@@ -0,0 +1,24 @@
+const email = document.querySelector('.form-js');
+const subscribeInput = document.getElementById('email');
+const subscribeInputValue = subscribeInput.value.trim();
+const errorMessage = document.getElementById('error-msg');
+
+
+
+
+email.addEventListener("submit", (e) => {
+    e.preventDefault();
+
+    if (subscribeInputValue === '') {
+        subscribeInput.style.border = "1px solid red";
+        subscribeInput.classList.add('error-input');
+    
+        errorMessage.innerHTML = "Valid email required";
+    }
+})
+
+//document.location='subscription.html'
+
+
+
+
